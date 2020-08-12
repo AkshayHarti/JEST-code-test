befe9a33ca3a90123636052a0e5741f0cf2070b8
Geethanjali Veetil <gtveetil@gmail.com>
2020-08-12 17:10:48 +1000

    Added solution for Pagination - Closes #3

--- src/03_pagination.js
+++ src/03_pagination.js
@@ -5,7 +5,23 @@
  * @param {Array<string>} pageData
  */
 function solution (pageNumber, itemsPerPage, pageData) {
+  if (!Number.isInteger(pageNumber) ||
+    !Number.isInteger(itemsPerPage) || itemsPerPage <= 0 ||
+    !Array.isArray(pageData)) {
+    throw new Error('Invalid Parameters')
+  }
 
+  // Default to 1 as the start page
+  const startPage = (pageNumber > 0) ? pageNumber : 1
+  const startIndex = (startPage - 1) * itemsPerPage
+  const endIndex = startIndex + itemsPerPage
+
+  // Page out of array bounds
+  if (startIndex > pageData.length - 1) {
+    return null
+  }
+
+  return pageData.slice(startIndex, endIndex)
 }
 
 const data = [
