9d3d015f431675a4db5550f0315e6290c41a72e3
Geethanjali Veetil <gtveetil@gmail.com>
2020-08-12 13:18:05 +1000

    Add solution to get first non repeating character - Closes #2

--- src/02_get-first-non-repeating-char.js
+++ src/02_get-first-non-repeating-char.js
@@ -5,7 +5,20 @@
  * @param {String} str
  */
 function solution (str) {
+  if (str == null) {
+    return null
+  }
+  if (typeof str !== 'string') {
+    throw new Error('Parameter should be a string')
+  }
 
+  const result = str
+    .replace(/\s/g, '') // Remove whitespaces
+    .split('')
+    .find((char, _, charArray) =>
+      charArray.indexOf(char) === charArray.lastIndexOf(char))
+
+  return result || null
 }
 
 module.exports = solution
