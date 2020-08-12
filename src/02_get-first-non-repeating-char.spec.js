7a04e1f40a8bbff60350b5ba3fb565c1aedc6136
Geethanjali Veetil <gtveetil@gmail.com>
2020-08-12 13:19:01 +1000

    Added tests to make the code coverage higher - part of fix for #5

--- src/02_get-first-non-repeating-char.spec.js
+++ src/02_get-first-non-repeating-char.spec.js
@@ -11,3 +11,24 @@ test('Test 2 - "persuit is a good place to work"', () => {
 test('Test 3 - "we have free coffee"', () => {
   expect(solution('we have free coffee')).toBe('w')
 })
+
+test('Test 4 - white space check - "hot doughnut"', () => {
+  expect(solution('hot doughnut')).toBe('d')
+})
+
+test('Test 5 - no unique characters - "she sells shells"', () => {
+  expect(solution('she sells shells')).toBeNull()
+})
+
+test('Test 6 - null value check', () => {
+  expect(solution(null)).toBeNull()
+})
+
+test('Test 7 - should throw an error when called with an incorrect parameter', () => {
+  // Arrange
+  const input = ['a', 'b', 'c']
+  const error = new Error('Parameter should be a string')
+
+  // Assert
+  expect(() => { solution(input) }).toThrow(error)
+})
